bc491dcbdfbfa28fe143bc8b4080dfabcdb46526
sachindimri788 <sachindimri788@gmail.com>
2023-06-18 10:11:16 +0530

    adding new file

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const header=document.getElementById('main-header');
+const addItem=document.getElementsByClassName('title');
+
+
+header.style.borderBottom="solid 3px #000";
+
+addItem.style.fontWeight="bold";
+
+addItem.style.color="green";
