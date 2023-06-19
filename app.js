0fbe6e156ee14acc0b2fdb38ad9b619515f53660
sachindimri788 <sachindimri788@gmail.com>
2023-06-19 08:08:25 +0530

    7

--- app.js
+++ app.js
@@ -28,7 +28,16 @@
 // }
 
 
-var list=document.querySelectorAll("li:nth-child(odd)");
+// var list=document.querySelectorAll("li:nth-child(odd)");
 
-for(var i=0;i<list.length;i++){
-    list[i].style.backgroundColor="green"; }
+// for(var i=0;i<list.length;i++){
+//     list[i].style.backgroundColor="green"; }
+
+
+// Add "Hello" before "Item Lister"
+var headerTitle = document.getElementById('header-title');
+headerTitle.innerHTML = 'Hello ' + headerTitle.innerHTML;
+
+// Add "Hello" before "Item 1"
+var item1 = document.querySelector('#items li:first-child');
+item1.innerHTML = 'Hello ' + item1.innerHTML;
